04430ac0558f79451d27484a162f890cba7c50bf
Tabitha <tabitha.kimani123@gmail.com>
2022-10-05 23:34:20 +0300

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,13 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Setup;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [Setup::class];
     /**
      * Define the application's command schedule.
      *
